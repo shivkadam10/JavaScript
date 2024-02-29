75e515612e56e20f0c2833488f795dc6c6a58785
shivkadam10 <85212407+shivkadam10@users.noreply.github.com>
2024-02-29 10:11:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Treats all JS code as newer version of JavaScript
+
+// alert(1 + 2); // If executed in browser then pop will occur with message 3
+
+let userName = "Shiv_Kadam_"
+let age = 21
+let isLoggedIn = true
+
+// Datatypes
+/*
+    Number
+    Bigint
+    String
+    Boolean
+    Null
+    Undefined
+    Symbol
+
+    Object
+*/
+
+console.log(typeof userName)  // typeof is used to get datatype of given object
+
+console.log(typeof null) // This will show object
+console.log(typeof undefined) // This will show undefined
